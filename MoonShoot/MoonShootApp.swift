cc4ab0fcf752a015e0a6da9c06f747c8c2414d1c
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-14 15:05:39 +0530

    Initial Commit

---
+++ MoonShoot/MoonShootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShootApp.swift
+//  MoonShoot
+//
+//  Created by Shashank B on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
